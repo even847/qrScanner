f442ca07c12dd1d2bd361517c304595c29fcc9f4
Edwin Aponte <aponte847@gmail.com>
2021-08-12 16:04:05 -0400

    creacion de models y servicios

--- src/app/pages/tab1/tab1.page.ts
+++ src/app/pages/tab1/tab1.page.ts
@@ -1,12 +1,53 @@
 import { Component } from '@angular/core';
+import { BarcodeScanner } from '@ionic-native/barcode-scanner/ngx';
+import { DataLocalService } from 'src/app/services/data-local.service';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  swiperOpts = {
+    allowSlidePrev: false,
+    allowSlideNext: false,
+  };
 
-  constructor() {}
+  constructor(
+    private barcodeScanner: BarcodeScanner,
+    private dataLocalService: DataLocalService
+  ) {}
 
+  ionViewDidEnter() {
+    console.log('ionViewDidEnter');
+  }
+
+  ionViewDidLeave() {
+    console.log('ionViewDidLeave');
+  }
+
+  ionViewWillEnter() {
+    console.log('ionViewWillEnter');
+    this.scan();
+  }
+
+  ionViewWillLeave() {
+    console.log('ionViewWillLeave');
+  }
+
+  scan() {
+    this.barcodeScanner
+      .scan()
+      .then((barcodeData) => {
+        console.log('Barcode data', barcodeData);
+
+        if ( !barcodeData.cancelled ) {
+          this.dataLocalService.guardarRegistro( barcodeData.format, barcodeData.text );
+        }
+      })
+      .catch((err) => {
+        console.log('Error', err);
+        this.dataLocalService.guardarRegistro( 'QRCode', 'https://fernando-herrera.com' );
+      });
+    }
 }
